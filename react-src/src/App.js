a8547089a583fb1963b3fe5b118605aaba2fbf31
freddiemixell <fmixell@gmail.com>
2018-10-08 18:10:36 -0400

    Working on SSR

--- react-src/src/App.js
+++ react-src/src/App.js
@@ -11,16 +11,16 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-      <div>
-          <Header/>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/blog" component={Blog} />
-              <Route path="/blog/:slug" component={BlogPost} />
-              <Route path="/:slug" component={Page} />
-            </Switch>
-          <Footer/>
-      </div>
+        <div>
+            <Header/>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/blog" component={Blog} />
+                <Route path="/blog/:slug" component={BlogPost} />
+                <Route path="/:slug" component={Page} />
+              </Switch>
+            <Footer/>
+        </div>
       </Router>
     );
   }
